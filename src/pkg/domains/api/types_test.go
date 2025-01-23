25e24a42c6334c99094e16e0bd12fd5011a7795a
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-01-23 20:40:29 +0000

    fix(api): update testing accordingly

--- src/pkg/domains/api/types_test.go
+++ src/pkg/domains/api/types_test.go
@@ -81,6 +81,9 @@ func TestGetResources(t *testing.T) {
 
 	apiReqName := "test"
 	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+
 		if r.Header.Get("Accept") != "application/json" {
 			w.WriteHeader(http.StatusBadRequest)
 		} else if _, ok := r.URL.Query()["label"]; !ok {
@@ -142,9 +145,85 @@ func TestGetResources(t *testing.T) {
 				"statuscode": 400,
 				"status":     "400 Bad Request",
 				"response":   nil,
-				"raw":        json.RawMessage(nil),
+				"raw":        nil,
 			}},
 			drs,
 		)
+	})
+}
+
+func TestGetResourcesTextHTML(t *testing.T) {
+	respBytes := []byte("<html><body>Healthcheck OK</body></html>")
+	// Parse the response for text/html if necessary
+	resp := string(respBytes)
+
+	apiReqName := "test"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+
+		if r.Header.Get("Accept") != "text/html" {
+			w.WriteHeader(http.StatusBadRequest)
+		} else if _, ok := r.URL.Query()["label"]; !ok {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write(respBytes)
+			require.NoError(t, err)
+		}
+	}))
+	defer svr.Close()
+
+	t.Run("pass", func(t *testing.T) {
+		api, err := CreateApiDomain(&ApiSpec{
+			Requests: []Request{
+				{
+					Name:   apiReqName,
+					URL:    svr.URL,
+					Params: map[string]string{"label": "test"},
+					Options: &ApiOpts{
+						Headers: map[string]string{"Accept": "text/html"},
+					},
+				},
+			},
+		})
+
+		require.NoError(t, err)
+		drs, err := api.GetResources(context.Background())
+		require.NoError(t, err)
+
+		want := types.DomainResources{
+			apiReqName: types.DomainResources{
+				// "raw" contains the HTML response
+				"raw":        resp,
+				"response":   nil,
+				"status":     "200 OK",
+				"statuscode": 200,
+			},
+		}
+		require.Equal(t, want, drs)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		api, err := CreateApiDomain(&ApiSpec{
+			Requests: []Request{
+				{
+					Name: apiReqName,
+					URL:  svr.URL,
+				},
+			},
+		})
+
+		require.NoError(t, err) // the spec is correct
+		drs, err := api.GetResources(context.Background())
+		require.NoError(t, err)
+		require.Equal(t, types.DomainResources{
+			apiReqName: types.DomainResources{
+				"statuscode": 400,
+				"status":     "400 Bad Request",
+				"response":   nil,
+				"raw":        nil,
+			},
+		},
+			drs)
 	})
 }
