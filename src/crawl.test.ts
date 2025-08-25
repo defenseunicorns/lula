463c7b1f97431280fc6be3c9926ea49a281115fa
Case Wylie <cmwylie19@defenseunicorns.com>
2025-08-25 11:20:12 -0400

    chore: unit test - format
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2025-Present The Lula2 Authors
 import { describe, it, expect } from "vitest";
-import { getPRContext } from "./crawl.js";
+
 describe("crawl", () => {
   it("getPRContext throws error if env vars are missing", () => {
     expect(true).toBe(true);
