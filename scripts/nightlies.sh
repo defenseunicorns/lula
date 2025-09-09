6a7d32c99981ad27a35af9a5e9fac0ed9ee13cbf
Case Wylie <cmwylie19@defenseunicorns.com>
2025-09-09 18:13:13 -0400

    chore: update script for pnpm
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- scripts/nightlies.sh
+++ scripts/nightlies.sh
@@ -6,7 +6,6 @@
 # Script to build and publish nightly versions of lula2.
 
 set -e
-npm install -g npm
 
 LATEST_VERSION=$(npx --yes lula2@latest --version 2>/dev/null)
 RAW_NIGHTLY_VERSION=$(npx --yes lula2@nightly --version 2>/dev/null || echo "none")
@@ -34,7 +33,7 @@ echo "FULL_VERSION=$FULL_VERSION" >> "$GITHUB_ENV"
 npm version --no-git-tag-version "$FULL_VERSION"
 
 pnpm install 
-nppm run build
-npm pack
+npm run build
+pnpm pack
 
-npm publish --tag "nightly"
+pnpm publish --tag "nightly" --no-git-checks
