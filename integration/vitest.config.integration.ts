65ba61ea9bd5974e7e721522fe1a5d138175f54b
Case Wylie <cmwylie19@defenseunicorns.com>
2025-09-09 14:34:49 -0400

    chore: vitest config fix
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- integration/vitest.config.integration.ts
+++ integration/vitest.config.integration.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		include: ['src/**/*.test.ts', 'hack/**/*.test.ts', 'cli/**/*.test.ts'],
+		include: ['integration/**/*.test.ts'],
 		exclude: ['node_modules', 'dist', 'coverage'],
 		coverage: {
 			provider: 'v8',
