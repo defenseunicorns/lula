0304191e7901ffdc90737ef40309ad6ee53ae8c4
Case Wylie <cmwylie19@defenseunicorns.com>
2025-09-11 10:49:58 -0400

    chore: add lcov reporter
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 		exclude: ['node_modules', 'dist', 'coverage'],
 		coverage: {
 			provider: 'v8',
-			reporter: ['text', 'html'],
+			reporter: ['text', 'html', 'lcov'],
 			reportsDirectory: './coverage',
 			include: ['src/**/*.ts', 'cli/**/*.ts'],
 			exclude: ['node_modules', 'dist', 'coverage']
